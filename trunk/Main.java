9db419ce1b95ecbbbd337e4fe449c1967e254bf0
mediaptera <mediaptera@b9618479-4fae-5d82-29bb-af35c98fba41>
2011-04-03 07:49:38 +0000

    Beginnings of GUI working\!

---
+++ trunk/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package squawkshell;
+
+/**
+ *
+ * @author JP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI g = new GUI();
+        g.setVisible(true);
+    }
+
+}
