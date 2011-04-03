60b4bd1e46ee05f87cdceb5e104eaaa6ca0e106a
mediaptera <mediaptera@b9618479-4fae-5d82-29bb-af35c98fba41>
2011-04-03 19:28:16 +0000

    This is final; forgot license on ci.java

--- trunk/CommandInterface.java
+++ trunk/CommandInterface.java
@@ -1,3 +1,24 @@
+/**
+ * Class to handle interactions with shells..
+ * 
+ * Copyright (C) 2011   Hoa Long Tam, Joshua Evenson, Ryan Abrams, Jessica Pan.
+ *
+ * This file is part of Squawk-Shell.
+ *
+ * Squawk-Shell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Squawk-Shell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Squawk-Shell.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
